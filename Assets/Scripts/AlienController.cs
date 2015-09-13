dd58e3f4ba59ac0663081924d3be95385cbeaac0
Ankit Patel <ankit01217@gmail.com>
2015-09-12 23:35:00 -0400

    changed alien controller

--- Assets/Scripts/AlienController.cs
+++ Assets/Scripts/AlienController.cs
@@ -6,13 +6,22 @@ public class AlienController : MonoBehaviour {
 	GameObject[] aliens;
 	AudioSource audioSource;
 	public AudioClip gameoverClip;
+	private GameObject planet;
+	private ParticleSystem planetPS;
 
 	// Use this for initialization
 	void Start () {
 		audioSource = GetComponent<AudioSource> ();
+		planet = GameObject.FindGameObjectWithTag("Planet");
+		planetPS = planet.GetComponent<ParticleSystem>();
+		planetPS.enableEmission = true;
 	}
 
 	public void killRandomAlien(){
+
+		if(planetPS.isPlaying) planetPS.Stop();
+		if(!planetPS.isPlaying) planetPS.Play();
+
 		aliens = (GameObject[])GameObject.FindGameObjectsWithTag ("Alien");
 		if (aliens != null && aliens.Length > 0) {
 			GameObject alien = aliens [Random.Range (0, aliens.Length)];
