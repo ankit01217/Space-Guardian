2379a3c17e9697048c6eb5750cf9ad24e2fcc70e
Sarah <sarahtan@cmu.edu>
2015-09-13 20:31:14 -0400

    Worked on calibration screen

---
+++ Assets/Scripts/Calibration/LiftCutOut.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LiftCutOut : MonoBehaviour {
+
+	public Camera cam;
+	Image image;
+
+	// Use this for initialization
+	void Start () {
+		image = GetComponent<Image> ();
+		SetSize ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void SetSize () {
+//		if (image) {
+//			float worldScreenHeight = (float)(cam.orthographicSize * 2.0);
+//			float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+//			//float newLocalScale = (worldScreenWidth / image.sprite.bounds.);
+//			transform.localScale = new Vector3(newLocalScale, newLocalScale, 1);
+//		}
+	}
+}
