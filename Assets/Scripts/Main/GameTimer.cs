5ac4d70ca381550c82045ae89139f30ad2a24eaf
Ankit Patel <ankit01217@gmail.com>
2015-09-14 19:43:36 -0400

    changes after playtest

--- Assets/Scripts/Main/GameTimer.cs
+++ Assets/Scripts/Main/GameTimer.cs
@@ -3,17 +3,19 @@ using System.Collections;
 using UnityEngine.UI;
 public class GameTimer : MonoBehaviour
 {
-
+	public Camera camera;
+	public GameObject inner,outer;
 	GameObject gameTimer;
 	public float timerSpeed = 0.01f;
 	public AudioClip timerAudio, gameEndAudio;
 	AudioSource audioSource;
 	bool isTimerAudioEnabled = false;
 	bool isGameOver = false;
+
 	// Use this for initialization
 	void Start ()
 	{
-	
+		 
 		audioSource = GetComponent<AudioSource> ();
 		gameTimer = GameObject.FindGameObjectWithTag ("GameTimer");
 		Vector3 localScale = gameTimer.transform.localScale;
@@ -21,6 +23,15 @@ public class GameTimer : MonoBehaviour
 		gameTimer.transform.localScale = newScale;
 	
 		InvokeRepeating ("updateTimer", 0.01f, 0.01f);
+		resizeTimer();
+	}
+
+	void resizeTimer(){
+
+		double width = Camera.main.orthographicSize * 2.0 * Screen.width / Screen.height;
+		float innerWidth = GetComponent<Collider> ().bounds.size.x;
+		outer.transform.localScale = new Vector3((float)width / innerWidth, 1, 1);
+
 	}
 
 
@@ -31,10 +42,7 @@ public class GameTimer : MonoBehaviour
 		float newXScale = Mathf.Clamp (localScale.x - Time.deltaTime * timerSpeed, 0, 1);
 		Vector3 newScale = new Vector3 (newXScale,localScale.y,localScale.z);
 		gameTimer.transform.localScale = newScale;
-		
-
-		gameTimer.transform.localScale = newScale;
-
+		inner.transform.localScale = newScale;
 
 		if (newXScale < 0.07f && isTimerAudioEnabled == false) {
 			Debug.Log (newXScale);
