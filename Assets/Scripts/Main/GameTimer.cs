ca1756432d0323f98b9ea3e12260f91a9bd1c93b
Ankit Patel <ankit01217@gmail.com>
2015-09-17 18:55:07 -0400

    changed GameTimer, Spaceship Generator

--- Assets/Scripts/Main/GameTimer.cs
+++ Assets/Scripts/Main/GameTimer.cs
@@ -6,22 +6,23 @@ public class GameTimer : MonoBehaviour
 {
 	public Text timerText;
 	public float timerSpeed;
-	public AudioClip timerAudio,gameEndAudio, flickerAudio, shieldCompleteAudio, shieldRefillAudio;
-	AudioSource audioSource;
-	bool isTimerAudioEnabled = false;
-	bool isGameOver = false;
-	bool isFlickerEnabled = false;
-	float timer = 0f;
-	bool isFlickering = false;
 	public GameObject shield;
-	private MeshRenderer shieldRenderer;
 	public float maxAlpha = 0.5f;
 	public float flickerPerc = 10f;
 	public Image tranTint;
+	public AudioClip timerAudio,gameEndAudio, flickerAudio, shieldCompleteAudio, shieldRefillAudio;
+	public static float timer = 90f;
+
+	MeshRenderer shieldRenderer;
+	AudioSource audioSource;
+	bool isTimerAudioEnabled = false;
+	bool isFlickerEnabled = false;
+	bool isFlickering = false;
+	SpaceshipController spaceshipController;
 
 	void Awake(){
 		tranTint.enabled = false;
-
+		timer = 0;
 		shieldRenderer = shield.GetComponent<MeshRenderer> ();
 		setShieldAlpha(0);
 		shield.SetActive (true);
@@ -31,6 +32,7 @@ public class GameTimer : MonoBehaviour
 	void Start ()
 	{
 		audioSource = GetComponent<AudioSource> ();
+		spaceshipController = GameObject.FindObjectOfType<SpaceshipController> ();
 		InvokeRepeating ("updateTimer", 0.01f, 0.01f);
 	}
 
@@ -49,24 +51,33 @@ public class GameTimer : MonoBehaviour
 			timer = flickerPerc;
 		}
 
-		if (isGameOver == false && isFlickering == false) {
+		if (GameManager.isGameOver == false && isFlickering == false) {
 			timerText.text = (int)timer + "% complete";
 			setShieldAlpha (timer);
 			
 		}
 
-		if (timer == 100f && isGameOver == false) {
+
+
+		if (timer == 100f && GameManager.isGameOver == false) {
 			//game ends here
 			//shwo end cinematic and shield completion animaion
 			Debug.Log ("Game Over");
-			isGameOver = true;
-			//audioSource.PlayOneShot (gameEndAudio);
-			onGameOverSuccess();
+			GameManager.isGameOver = true;
+			spaceshipController.activateLastPhase();
+
+			LeanTween.alpha (shield, 0, 0.01f);
+			audioSource.PlayOneShot (shieldCompleteAudio);
+			Color newColor = new Color (1, 0.90f, 91/255f, 0.35f);
+			shieldRenderer.material.color = newColor;
+			LeanTween.alpha (shield, 0.3f, 0.5f).setEase(LeanTweenType.easeOutCirc);
+			Invoke("startTransition",5f);
 
 		}
 
 
 	}
+
 
 	void setShieldAlpha(float alpha){
 
@@ -86,28 +97,14 @@ public class GameTimer : MonoBehaviour
 
 	void endShieldFlickerAnimation(){
 		isFlickering = false;
-
 		audioSource.PlayOneShot (shieldRefillAudio);
 
 	}
 
-	void onGameOverSuccess(){
-		Debug.Log ("onGameOverSuccess");
-
-		LeanTween.alpha (shield, 0, 0.01f);
-		audioSource.PlayOneShot (shieldCompleteAudio);
-		Color newColor = new Color (1, 0.90f, 91/255f, 0.35f);
-		shieldRenderer.material.color = newColor;
-		LeanTween.alpha (shield, 0.3f, 0.5f).setEase(LeanTweenType.easeOutCirc);
-	
-		tranTint.enabled = true;
-		Invoke("startTransition",0.5f);
-
-
-	}
 
 	void startTransition(){
-		Invoke("startEndCinematic",0.3f);
+		tranTint.enabled = true;
+		Invoke("startEndCinematic",1f);
 
 	}
 
@@ -115,8 +112,6 @@ public class GameTimer : MonoBehaviour
 		Application.LoadLevel(2);
 
 	}
-
-
 
 	// Update is called once per frame
 	void Update ()
