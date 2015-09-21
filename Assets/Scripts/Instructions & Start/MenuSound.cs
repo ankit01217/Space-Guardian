1ead3bb9ccb37a37e8b56d44852c905a99296d79
Ankit Patel <ankit01217@gmail.com>
2015-09-21 12:03:04 -0400

    fixed instructions and start scene music pause

---
+++ Assets/Scripts/Instructions & Start/MenuSound.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSound : MonoBehaviour {
+
+	static bool AudioBegin = false; 
+	AudioSource audio;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Awake()
+	{
+		audio = GetComponent<AudioSource>();
+		if (!AudioBegin) {
+			audio.Play ();
+			DontDestroyOnLoad (gameObject);
+			AudioBegin = true;
+		} 
+	}
+	void Update () {
+		if(Application.loadedLevelName == "Instructions")
+		{
+			//audio.Stop();
+			AudioBegin = false;
+		}
+	}
+
+
+}
